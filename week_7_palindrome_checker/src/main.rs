554edf7ee1d8a3d1010737c13298cd096f017591
MADMAN-Modding <mad@kmdtech.net>
2024-06-09 19:39:59 -0400

    Palindrome checker

--- week_7_palindrome_checker/src/main.rs
+++ week_7_palindrome_checker/src/main.rs
@@ -1,13 +1,32 @@
 use std::io;
 
 fn main() {
-    let mut input = String::new();
+    // Instantiates the input String
+    let mut input:String = String::new();
 
+    // Friendly text
     println!("Enter a string to see if it's palindrome!");
 
+    // Sets the value of input and checks for errors
     io::stdin()
         .read_line(&mut input)
         .expect("Unable to parse String");
 
-    println!("{}", input.chars().count());
+    // Trims any tailing spaces and such
+    input = input.trim().to_string().to_lowercase();
+
+    // Version of input with no spaces
+    let clean_input:String = input.chars()
+        .filter(|c| c.is_alphabetic())
+        .collect();
+
+    // Instantiates the reversed_input String for the reversed input
+    let reversed_input:String = clean_input.chars().rev().collect();
+
+    // Checks if the reversed_input is equal to the clean_input
+    if reversed_input == clean_input {
+        println!("{} is palindrome!", input);
+    } else {
+        println!("{} is not palindrome", input);
+    }
 }
